22206cc1c8c7d29ec350e7e65af663a19ef10268
Ben Baumer <ben.baumer@gmail.com>
2026-02-06 16:04:48 -0500

    remove superfluous data sets

--- data-raw/combine.R
+++ data-raw/combine.R
@@ -29,7 +29,8 @@ wnba_gl <- wnba_gl |>
   left_join(
     caitlin_clark |> mutate(is_cc = MIN > 0) |> select(GAME_ID, is_cc), 
     by = join_by(GAME_ID)
-  )
+  ) |>
+  mutate(is_cc = if_else(is.na(is_cc), 0, is_cc))
 
 
 
@@ -37,3 +38,14 @@ wnba_gl <- wnba_gl |>
 
 
 write_rds(wnba_gl, here::here("data/wnba_gl.rds"))
+
+
+
+# Consider this alternative model specification
+mod_attendance <- lm(
+  attendance ~ rolling_wins_44 + factor(wday(GAME_DATE)) + factor(month(GAME_DATE)) + TEAM_NAME + SEASON_ID + is_cc,
+  data = wnba_gl
+)
+
+summary(mod_attendance)
+
